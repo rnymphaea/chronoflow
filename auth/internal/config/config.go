4bbe56c5c3c322ceb9b6d0c35ee3f0e7ab182325
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-08-02 02:09:32 +0300

    server config updated

--- auth/internal/config/config.go
+++ auth/internal/config/config.go
@@ -1,11 +1,22 @@
 package config
 
 import (
+	"time"
+
 	"github.com/caarlos0/env/v11"
 )
 
 type ServerConfig struct {
-	GRPCAddress string `env:"GRPC_ADDRESS"`
+	Address string `env:"ADDRESS" envDefault:":50051"`
+
+	MaxConcurrentStreams uint32 `env:"MAX_CONCURRENT_STREAMS" envDefault:"100"`
+
+	MaxConnectionIdle     time.Duration `env:"MAX_CONN_IDLE" envDefault:"30m"`
+	MaxConnectionAge      time.Duration `env:"MAX_CONN_AGE" envDefault:"2h"`
+	MaxConnectionAgeGrace time.Duration `env:"MAX_CONN_AGE_GRACE" envDefault:"3m"`
+
+	Time    time.Duration `env:"TIME" envDefault:"2h"`
+	Timeout time.Duration `env:"TIMEOUT" envDefault:"20s"`
 }
 
 func LoadServerConfig() (*ServerConfig, error) {
