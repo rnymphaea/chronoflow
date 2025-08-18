b6477142a311e5e159253d21a127cf0c4fe384f4
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-08-18 22:47:17 +0300

    added cache config

---
+++ auth/internal/config/cache.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"time"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type RedisConfig struct {
+	Addr     string `env:"REDIS_ADDR,required"`
+	Password string `env:"REDIS_PASSWORD,required"`
+	User     string `env:"REDIS_USER,required"`
+	DB       int    `env:"REDIS_DB" envDefault:"0"`
+
+	MaxRetries  int           `env:"REDIS_MAX_RETRIES" envDefault:"3"`
+	DialTimeout time.Duration `env:"REDIS_DIAL_TIMEOUT" envDefault:"10s"`
+	Timeout     time.Duration `env:"REDIS_TIMEOUT" envDefault:"5s"`
+	TTL         time.Duration `env:"REDIS_TTL" envDefault:"24h"`
+}
+
+func LoadRedisConfig() (*RedisConfig, error) {
+	var cfg RedisConfig
+	if err := env.Parse(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
