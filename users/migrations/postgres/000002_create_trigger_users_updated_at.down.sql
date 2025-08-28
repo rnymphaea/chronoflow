f35756371163a812977f19a51da313bb2b004180
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-08-28 17:21:48 +0300

    users: initial migrations added

---
+++ users/migrations/postgres/000002_create_trigger_users_updated_at.down.sql
@@ -0,0 +1,2 @@
+DROP TRIGGER IF EXISTS trigger_set_updated_at ON users;
+DROP FUNCTION IF EXISTS set_updated_at();
---
+++ users/migrations/postgres/000002_create_trigger_users_updated_at.up.sql
@@ -0,0 +1,12 @@
+CREATE OT REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at := NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_set_updated_at
+BEFORE UPDATE ON users 
+FOR EACH ROW 
+EXECUTE FUNCTION set_updated_at();
